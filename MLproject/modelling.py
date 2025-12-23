8126234acb7a05338b0665f83c3d996ad3a69561
dhekmass12 <dimas.tri01@ui.ac.id>
2025-12-24 02:28:17 +0700

    DONE

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -10,7 +10,7 @@ import sys
 # dagshub.init(repo_owner='dhekmass12', repo_name='modelling_without_tuning', mlflow=True)
 
 mlflow.set_tracking_uri("http://127.0.0.1:5000/")
-# mlflow.set_experiment("Modelling Without Tuning")
+mlflow.set_experiment("Modelling Without Tuning")
 
 df = pd.read_csv("dataset_preprocessing.csv")
 X = df.drop(['Failure Type'], axis=1)
